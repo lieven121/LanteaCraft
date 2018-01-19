606eb20b29c5ec10868b8229dce6e5810eb1b85b
lieven121 <ldetroch@outlook.com>
2018-01-19 21:51:45 +0100

    Things i changed but forgot to push with correct topic
    DOORS AGAIN??!!
    ring animation
    blockfilter
    added other dhds
    instanstruct
    added sound of other versions to dhd

--- src/main/java/lc/client/render/animations/RingSpinAnimation.java
+++ src/main/java/lc/client/render/animations/RingSpinAnimation.java
@@ -2,6 +2,7 @@ package lc.client.render.animations;
 
 import net.minecraft.world.World;
 import lc.LCRuntime;
+import lc.api.stargate.StargateType;
 import lc.client.animation.Animation;
 import lc.client.render.gfx.particle.GFXDust;
 import lc.common.base.LCTile;
@@ -16,21 +17,25 @@ public class RingSpinAnimation extends Animation {
 		super(duration, resample, new RunnableTileCallback() {
 			@Override
 			public void run(LCTile tile) {
-				tile.mixer().replayChannel("spin");
-				Vector3[] chevrons = ((TileStargateBase) tile).getChevronBlocks();
-				World world = tile.getWorldObj();
-				for (int i = 0; i < chevrons.length; i++) {
-					if (world.rand.nextInt(2) == 1) {
-						Vector3 chevron = chevrons[i];
-						GFXDust spark = new GFXDust(world, chevron.x, chevron.y, chevron.z, 0.024f, 0.0f, 0.02f);
-						LCRuntime.runtime.hints().particles().placeParticle(world, spark);
+				if ((StargateType.fromOrdinal(tile.getBlockMetadata()) == StargateType.STANDARD) | (StargateType.fromOrdinal(tile.getBlockMetadata()) == StargateType.NOX) ) {
+					tile.mixer().replayChannel("spin");
+					Vector3[] chevrons = ((TileStargateBase) tile).getChevronBlocks();
+					World world = tile.getWorldObj();
+					for (int i = 0; i < chevrons.length; i++) {
+						if (world.rand.nextInt(2) == 1) {
+							Vector3 chevron = chevrons[i];
+							GFXDust spark = new GFXDust(world, chevron.x, chevron.y, chevron.z, 0.024f, 0.0f, 0.02f);
+							LCRuntime.runtime.hints().particles().placeParticle(world, spark);
+						}
 					}
-				}
+				} else
+					tile.mixer().replayChannel("spin_alt");
 			}
 		}, new RunnableTileCallback() {
 			@Override
 			public void run(LCTile tile) {
 				tile.mixer().stopChannel("spin");
+				tile.mixer().stopChannel("spin_alt");
 			}
 		});
 		addProperty("ring-rotation", start, end, InterpolationMode.SIN);
